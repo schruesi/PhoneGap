85a881341b20ea0625b344a69ba9ff38a9513473
schruesi <simon.schruefer@web.de>
2014-07-02 17:10:46 +0200

    Update spiel.js

--- nbproject/spiel.js
+++ nbproject/spiel.js
@@ -6,3 +6,24 @@
 $("#document").ready()
 $("body").html("Du bist dran!");
 );
+$(document).ready(function(){
+    $("body").html("<div class=' ueberschrift'>Mein Spiel</div>");
+    $("body").append("Gleich gehts los");
+    $("body").append("<br> Mein BMI-Rechner:<br>Größe(in m!):<input id='groesse' type='text'/>");
+    $("body").append("<br>Gewicht(in kg!):<input id='gewicht' type='text'/>");
+    $("body").append("<br> <input id='button' type='button' value='Berechnen'");
+    $("body").append("<div id='ausgabe' />");
+    
+    $("#button").click(function(){
+        var groesse=parseFloat($("#groesse").val(),10);
+        var gewicht=parseFloat($("#gewicht").val(),10);
+        
+        var BMI=gewicht/(groesse*groesse);
+        
+        $("#ausgabe").html("Dein BMI ist:"+BMI)
+        
+       });
+    
+    });    
+   
+// höhö alle leute sind fett ]:)  bmi formel: gewicht (in kg)/ grösse^2
